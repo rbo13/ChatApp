29953c9395c509dc4b1d7445e0d97f15f397a29d
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-04-17 17:05:35 +0800

    Added method doLogin

--- app/src/main/java/com/example/b0kn0y/chatapp/MainActivity.java
+++ app/src/main/java/com/example/b0kn0y/chatapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.b0kn0y.chatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doLogin(View view) {
+        Toast.makeText(this, "Do login...", Toast.LENGTH_SHORT).show();
+        loadChatView();
+    }
+
+    private void loadChatView() {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
